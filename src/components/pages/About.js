436637e1163997d8c231ecaf621b81de97e841aa
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-09 12:44:05 -0600

    css JSX test

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import '../../styles/aboutStyles.css'
+
+const styles = {
+  headerStyle: {
+    background: 'red',
+  },
+  headingStyle: {
+    fontSize: '100px',
+  },
+};
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Testing About... Can you read this?
-      </p>
+    <div style={styles.headerStyle} className="about-container">
+        <h1 style={styles.headingStyle} className="about-title">About Page</h1>
+        <p>
+          Text here
+        </p>
     </div>
   );
 }
