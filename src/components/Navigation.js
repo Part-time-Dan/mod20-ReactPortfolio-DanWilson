d4353bb0a9578cbe2eb01ed97e8bc508d8093df1
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-11 03:19:26 -0600

    resume link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,51 +5,51 @@ function Navigation({ currentPage, handlePageChange }) {
 
   return (
 
-      <div className="nav-container">
-        <ul className="nav-list">
+    <div className="nav-container">
+      <ul className="nav-list">
 
-          <li className="nav-item">
-            <a
-              href="#about"
-              onClick={() => handlePageChange('About')}
-              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-            >
-              About Me
-            </a>
-          </li>
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About Me
+          </a>
+        </li>
 
-          <li className="nav-item">
-            <a
-              href="#portfolio"
-              onClick={() => handlePageChange('Portfolio')}
-              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-            >
-              Portfolio
-            </a>
-          </li>
+        <li className="nav-item">
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange('Portfolio')}
+            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+          >
+            Portfolio
+          </a>
+        </li>
 
-          <li className="nav-item">
-            <a
-              href="#resume"
-              onClick={() => handlePageChange('Resume')}
-              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-            >
-              Resume
-            </a>
-          </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </a>
+        </li>
 
-          <li className="nav-item">
-            <a
-              href="#contact"
-              onClick={() => handlePageChange('Contact')}
-              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-            >
-              Contact
-            </a>
-          </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </a>
+        </li>
 
-        </ul>
-      </div>
+      </ul>
+    </div>
 
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 export default function Resume() {
+
+  const pdfUrl = 'https://drive.google.com/file/d/1Sy8buvBVqsZG9zUCPMmmCXyoyNfJ3VJl/view?usp=sharing'; 
+
   return (
     <div>
-      <h1>My Resume</h1>
-      <p>
-        A list of things
-      </p>
+      <h2>My Resume</h2>
+      <p>View or download my resume:</p>
+      <a href={pdfUrl} download="DanWilson-dev-resume.pdf">
+        Download Resume
+      </a>
     </div>
   );
 }
