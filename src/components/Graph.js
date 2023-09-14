980340e2e1b4d891c3bcc027f4f4d4ac6c105f59
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-14 15:49:54 -0600

    resume skills chart

---
+++ src/components/Graph.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import '../styles/graphStyles.css';
+
+
+export default function Graph() {
+    const skillsData = [
+        // adjust number values to change progress bar
+        {
+          category: 'Development',
+          skills: [
+            { name: 'HTML5', progress: 70 },
+            { name: 'CSS', progress: 50 },
+            { name: 'JavaScript + jQuery', progress: 60 },
+            { name: 'React', progress: 40 },
+            { name: 'Express + Nodejs', progress: 50 },
+            { name: 'MySQL + MongoDB', progress: 40 },
+          ],
+        },
+        {
+          category: 'Productivity',
+          skills: [
+            { name: 'Git + Github', progress: 60 },
+            { name: 'Heroku', progress: 40 },
+            { name: 'Microsoft Office', progress: 70 },
+          ],
+        },
+        {
+          category: 'Design',
+          skills: [
+            { name: 'Blender', progress: 25 },
+            { name: 'Illustrator', progress: 50 },
+            { name: 'User Experience', progress: 70 },
+          ],
+        },
+      ];
+    
+      return (
+        <div className="skills">
+          <div className="charts">
+            {skillsData.map((categoryData, index) => (
+              <div key={index} className={`chart chart-${categoryData.category.toLowerCase()}`}>
+                <div className="chart-background"></div>
+                <span className="chart-title">{categoryData.category}</span>
+                <ul className="chart-horiz no-bullet-points">
+                  {categoryData.skills.map((skill, skillIndex) => (
+                    <li key={skillIndex} className="chart-bar">
+                      <div className="chart-progress">
+                        <div className="chart-progress-bar" style={{ width: `${skill.progress}%` }}>
+                          <span className="chart-progress-label">{skill.progress}%</span>
+                          <span className="chart-label">{skill.name}</span>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
         <img src="/assets/business-cat.jpg" alt="This is not actually me. It is, in fact, a business cat." className="about-image" />
         <p className='about-content'>
           I live in the greater Salt Lake City metropolitan area. I work full-time while attending coding bootcamp through the University of Utah part-time, with a focus on full-stack web development. I enjoy solving puzzles
-          and working with computers so coding seemed like a fun opportunity that I'd like to turn into a career some day soon. <br/>
+          and working with computers so coding seemed like an exciting opportunity that I'd like to turn into a career some day soon. <br/>
           <br/>
           So far, I have mainly studied MERN stack through class. But I am interested in branching out into other coding languages for fun projects. 
           This portfolio you're visiting right now is my introduction to React.js.<br/> 
