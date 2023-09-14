27c39f95d8689f9f9e272f42e0ae21251617d854
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-14 11:58:27 -0600

    contacts form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,15 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/contactStyles.css'
 import '../../styles/pageStyles.css'
 
+export default function Contact({ setCurrentPage }) {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-export default function Contact() {
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    // validate empty fields and set errors
+    const newErrors = { ...errors };
+    if (value.trim() === '') {
+      newErrors[name] = `${name} is required`;
+    } else {
+      delete newErrors[name]; // remove error if field is not empty
+    }
+    setErrors(newErrors);
+  };
+
+  const handleMouseOut = (e) => {
+    const { name, value } = e.target;
+    // validate the field when the mouse leaves, trigger with onMouseOut in JSX
+    if (value.trim() === '') {
+      const newErrors = { ...errors };
+      newErrors[name] = `${name} is required`;
+      setErrors(newErrors);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // validate empty fields before submitting
+    const newErrors = {};
+    for (const field in formData) {
+      if (formData[field].trim() === '') {
+        newErrors[field] = `${field} is required`;
+      }
+    }
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+
+      console.log('Form submitted:', formData);
+
+      setFormData({
+        name: '',
+        email: '',
+        message: '',
+      });
+      setErrors({});
+    }
+  };
+
   return (
-    <div className='page-container'>
+    <div className="page-container">
       <h2 className='page-title'>Contact Me</h2>
-      <p className='contact-content'>
-        Here is my email contact info
-      </p>
+      <div className='contact-container'>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              onMouseOut={handleMouseOut}
+            />
+            {errors.name && <p className="error">{errors.name}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              onMouseOut={handleMouseOut}
+            />
+            {errors.email && <p className="error">{errors.email}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              onMouseOut={handleMouseOut}
+            />
+            {errors.message && <p className="error">{errors.message}</p>}
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,3 @@
-// import React, { useEffect, useRef } from 'react';
 import React from 'react';
 import '../../styles/portfolioStyles.css';
 import '../../styles/pageStyles.css';
