30fc70c9114709d6ceefff553623d642f11b7b0b
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-14 15:49:54 -0600

    resume skills chart

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import '../../styles/resumeStyles.css'
 import '../../styles/pageStyles.css'
+import Graph from '../Graph';
+
+
+
 
 export default function Resume() {
 
   const pdfUrl = 'https://drive.google.com/file/d/1Sy8buvBVqsZG9zUCPMmmCXyoyNfJ3VJl/view?usp=sharing';
 
   return (
-    <div className='page-container'>
-      <h2 className='page-title'>My Resume</h2>
-      <p className='resume-content'>View or download my resume:</p>
-      <a className='download-btn' href={pdfUrl} download="DanWilson-dev-resume.pdf">
-        Download Resume
-      </a>
-    </div>
+      <div className='page-container'>
+        <div>
+          <h2 className='page-title'>My Resume</h2>
+          <p className='resume-content'>View or download my resume:</p>
+          <a className='download-btn' href={pdfUrl} download="DanWilson-dev-resume.pdf">
+            Download Resume
+          </a>
+          
+        </div>
+        <div className='graph-container'>
+        <h3>Skills</h3>
+        <Graph />
+        </div>
+      </div>
   );
 }
